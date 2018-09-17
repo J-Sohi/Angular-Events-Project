20278e9068b59c2b988a0a47d1c699af0f320b6c
jasdip.sohi <jasdip.sohi@fdmgroup.com>
2018-09-17 15:58:40 -0400

    Completed section "Validating Template-Based Forms" from module "Collecting Data with Angular Forms and Validation"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { createEventComponent } from './events/create-event.component';
 import { Error404Component } from './errors/404.component';
 import { EventRouteActivator } from './events/event-details/event-route-activator.service';
 import { EventListResolver } from './events/events-list-resolver.service';
+import { AuthService } from './user/auth.service';
 
 @NgModule({
   declarations: [
@@ -33,6 +34,7 @@ import { EventListResolver } from './events/events-list-resolver.service';
               ToastrService,
               EventRouteActivator,
               EventListResolver,
+              AuthService,
               {
                 provide: 'canDeactivateCreateEvent', 
                 useValue: checkDirtyState
